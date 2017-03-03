21f5f5250ea298ff133385bbee0c7ca924fec014
helena <helena@waxym.com>
2017-03-03 11:04:26 +0100

    first

--- app/src/main/java/com/helenacorp/smiley/MainActivity.java
+++ app/src/main/java/com/helenacorp/smiley/MainActivity.java
@@ -1,21 +1,13 @@
 package com.helenacorp.smiley;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.support.v7.app.AppCompatActivity;
 
-import com.cipherthinkers.shapeflyer.ShapeFlyer;
-import com.cipherthinkers.shapeflyer.flyschool.FPoint;
-import com.cipherthinkers.shapeflyer.flyschool.FlyBluePrint;
-import com.cipherthinkers.shapeflyer.flyschool.FlyPath;
 import com.nacho91.panningview.HorizontalPanning;
 import com.nacho91.panningview.PanningView;
 import com.thunderrise.animations.FlipAnimation;
 import com.thunderrise.animations.PulseAnimation;
 import com.thunderrise.animations.RotateAnimation;
-import com.thunderrise.animations.ShakeAnimation;
-
-import static com.helenacorp.smiley.R.drawable.jj21;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,10 +30,9 @@ public class MainActivity extends AppCompatActivity {
                 .setRepeatCount(PulseAnimation.INFINITE)
                 .setDuration(2000)
                 .start();
-       /* RotateAnimation.create().with(JJ)
+        RotateAnimation.create().with(JJ)
                 .setRepeatCount(RotateAnimation.INFINITE)
-                .start(); */
-
+                .start();
 
     }
 }
